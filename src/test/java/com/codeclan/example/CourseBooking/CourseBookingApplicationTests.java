c718f7b357f071621172e92571c219f1bf11d6cb
Iain Tooley <iaintooley1@gmail.com>
2019-12-05 11:31:30 +0000

    added test

--- src/test/java/com/codeclan/example/CourseBooking/CourseBookingApplicationTests.java
+++ src/test/java/com/codeclan/example/CourseBooking/CourseBookingApplicationTests.java
@@ -1,13 +1,38 @@
 package com.codeclan.example.CourseBooking;
 
+import com.codeclan.example.CourseBooking.models.Course;
+import com.codeclan.example.CourseBooking.repositories.Bookingrepositories.BookingRepository;
+import com.codeclan.example.CourseBooking.repositories.Courserepositories.CourseRepository;
+import com.codeclan.example.CourseBooking.repositories.customerrepositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CourseBookingApplicationTests {
 
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Autowired
+	BookingRepository bookingRepository;
+
+	@Autowired
+	CourseRepository courseRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canGetAllCoursesWithGivenRating(){
+		List<Course> foundCourse = courseRepository.getAllCoursesForStarRating(5);
+		assertEquals(1, foundCourse.size());
+		assertEquals("Intro to Python", foundCourse.get(0).getName());
 	}
 
 }
